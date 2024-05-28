1f3adc63113daa08b9b801f848a68f19d9ae9c3d
Dylan <dylanrhanson@gmail.com>
2024-05-28 15:01:14 -0700

    clean up folders

--- data/progs/zip_folder.py
+++ data/progs/zip_folder.py
@@ -2,16 +2,16 @@ import os
 from zipfile import ZipFile
 
 # Create object of ZipFile
-with ZipFile('../config.zip', 'w', 0) as zip_object:
+with ZipFile('./data/config.zip', 'w', 0) as zip_object:
    # Traverse all files in directory
-   for folder_name, sub_folders, file_names in os.walk('../config'):
+   for folder_name, sub_folders, file_names in os.walk('./data/config'):
       for filename in file_names:
          # Create filepath of files in directory
          file_path = os.path.join(folder_name, filename)
          # Add files to zip file
          zip_object.write(file_path, os.path.basename(file_path))
 
-if os.path.exists('../config.zip'):
+if os.path.exists('./data/config.zip'):
    print("ZIP file created")
 else:
    print("ZIP file not created")
